fe8d310f86648a5aee3b2ca7da69b497255bbbc3
Andy Buckley <andy@insectnation.org>
2015-05-03 13:04:29 +0100

    Remove unnecessary dlopen flag tweaking: there's no dlopen calls in Professor

--- pyext/professor2/__init__.py
+++ pyext/professor2/__init__.py
@@ -1,12 +1,1 @@
-import sys
-try:
-    import ctypes
-    sys.setdlopenflags(sys.getdlopenflags() | ctypes.RTLD_GLOBAL)
-    del ctypes
-except:
-    import dl
-    sys.setdlopenflags(sys.getdlopenflags() | dl.RTLD_GLOBAL)
-    del dl
-del sys
-
 from professor2.core import *
