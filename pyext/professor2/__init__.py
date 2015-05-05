22a31eb30ca28dbc9a6a1a596f488f0252544e27
Andy Buckley <andy@insectnation.org>
2015-05-05 13:31:24 +0100

    Move Sampler from prof-sample into professor2/__init__.py. Add a --veto option to prof-sample, for a user-specified vetoing function.

--- pyext/professor2/__init__.py
+++ pyext/professor2/__init__.py
@@ -3,6 +3,65 @@ from professor2.core import *
 
 # TODO: move this stuff into submodules:
 
+## Define a sampler type
+class Sampler(object):
+
+    def __init__(self, low, high, bias=None):
+        self.low = float(low)
+        self.high = float(high)
+        self.f, self.invf = None, None
+        if bias:
+            ## Import clever machinery
+            try:
+                import sympy as sp
+                from sympy.abc import x, y
+                import numpy as np
+            except ImportError:
+                print "Bias functions require SymPy and NumPy to be installed... exiting"
+                exit(1) #< TODO: don't exit from inside a lib function...
+            ## Make transformation and its inverse
+            try:
+                #print bias
+                f_expr = sp.sympify(bias)
+            except sp.SympifyError, e:
+                print "Bias function could not be parsed by SymPy:"
+                print e
+                exit(1) #< TODO: don't exit from inside a lib function...
+            try:
+                finv_exprs = sp.solve(sp.Eq(y, f_expr), x)
+                finv_expr = finv_exprs[0]
+                #print f_expr, finv_exprs
+                self.f = sp.lambdify(x, f_expr, "numpy")
+                self.finv = sp.lambdify(y, finv_expr, "numpy")
+                self.lowf, self.highf = self.f(self.low), self.f(self.high)
+            except Exception, e:
+                print "Bias function could not be used/inverted by SymPy:"
+                print e
+                exit(1) #< TODO: don't exit from inside a lib function...
+
+    def shoot(self):
+        import random
+        if not self.f:
+            ## Just uniform sampling between low..high
+            val = random.uniform(self.low, self.high)
+        else:
+            ## Uniform sample in transformed space, and transform the result back
+            valf = random.uniform(self.lowf, self.highf)
+            val = self.finv(valf)
+        return val
+
+    def __call__(self):
+        return self.shoot()
+
+## Test biased sampler machinery
+# s = Sampler(1, 10, "exp(x)")
+# import yoda
+# h = yoda.Histo1D(20, 0, 10)
+# for _ in xrange(10000):
+#     h.fill( s() )
+# yoda.plot(h, "foo.pdf")
+
+
 class Histo(object):
 
     def __init__(self, bins=None):
