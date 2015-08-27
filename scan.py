925207d1dff3e4c4a6a88a3965ffc5337a614ef3
Andy Buckley <andy@insectnation.org>
2015-08-27 16:53:46 +0100

    Convert scan function to use yield and giving up on function auto-passing since iminuit's Minuit object can't be augmented with a new method

--- scan.py
+++ scan.py
@@ -18,23 +18,36 @@ import numpy as np
 # def f(**kwargs):
 #     print kwargs
 def f(a,b,c):
-    print a,b,c
+    return a**2 + 0.5*b**2 - c
 
 def scan(*args, **kwargs):
     vec = kwargs.get("vec", [])
     if args:
-        #print "@", len(args) #, args
+        # print "@", len(args) #, args
         var, npts, low, high = args[0]
         for val in np.linspace(low, high, npts):
             newargs = args[1:]
             newvec = vec + [(var, val)]
             newkwargs = kwargs
             newkwargs["vec"] = newvec
-            scan(*newargs, **newkwargs)
+            for i in scan(*newargs, **newkwargs):
+                yield i
+            # yield from scan(*newargs, **newkwargs):
     else:
-        fn = kwargs.get("fn", None)
-        fn(**dict(vec))
+        # print vec
+        yield vec
+        # yield dict(vec)
+        # fn = kwargs.get("fn", None)
+        # fn(**dict(vec))
 
-scan(["a", 3, 0, 1],
-     ["b", 5, 0, 4],
-     ["c", 2, 0, 2], fn=f)
+gen = scan(["a", 3, 0, 1],
+           ["b", 5, 0, 4],
+           ["c", 2, 0, 2]) #, fn=f)
+best = (None, None)
+for v in gen:
+    val = f(**dict(v))
+    print val
+    if best[1] is None or val < best[1]:
+        best = (v,val)
+        print "New best:", v, "with f =", val
+print "Absolute best =", best
