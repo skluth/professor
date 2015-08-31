3d26cdd520f93ff4afb09a9874a8158e951f0428
Andy Buckley <andy@insectnation.org>
2015-09-01 00:52:45 +0100

    Add recursive scangrid generator function, and other tweaks.

--- pyext/professor2/minimize.py
+++ pyext/professor2/minimize.py
@@ -4,28 +4,23 @@
 # TODO: support iminuit, which accepts/rejects different kwargs, and has no scan function
 # TODO: do we need a prof.Minuit wrapper on the wrappers?
 Minuit, MinuitError = None, Exception
-# try:
-#     from iminuit import Minuit, MinuitError
-# except ImportError:
 try:
-    from minuit import Minuit, MinuitError
+    from iminuit import Minuit, MinuitError
 except ImportError:
     try:
-        from minuit2 import Minuit2 as Minuit
-        from minuit2 import MinuitError
+        from minuit import Minuit, MinuitError
     except ImportError:
-        pass #print "Couldn't import a minimizer"
+        try:
+            from minuit2 import Minuit2 as Minuit
+            from minuit2 import MinuitError
+        except ImportError:
+            pass #print "Couldn't import a minimizer"
 
 
 def mk_fitfunc(fname, pnames):
     """
     Dynamically make a fit function for the given param names, to be passed to Minuit.
     """
-    funcargs = ", ".join(pnames)
-    funcdef = "def profGoF("
-    funcdef += funcargs
-    funcdef += "): "
-    funcdef += "return %s(["%fname
-    funcdef += funcargs
-    funcdef += "])"
+    fargs = ", ".join(pnames)
+    funcdef = "def profGoF({fargs}): return {fname}([{fargs}])".format(fargs=fargs, fname=fname)
     return funcdef
--- pyext/professor2/params.py
+++ pyext/professor2/params.py
@@ -1,5 +1,35 @@
 # -*- python -*-
 
+# TODO: remove numpy dependence... or have a pure-Python fall back?
+import numpy as np
+
+def scangrid(*args, **kwargs):
+    """
+    Generator function which returns lists of parameter (name, value)
+    2-tuples via yields (i.e. in a for-expression).
+
+    The arguments are 4-tuples of (name, numpts, low, high) for each
+    parameter; the generator iterations will then visit each of the
+    lattice points in a grid with num points between low..high in each
+    param.
+    """
+    vec = kwargs.get("vec", [])
+    if args:
+        # print "@", len(args) #, args
+        var, npts, low, high = args[0]
+        for val in np.linspace(low, high, npts):
+            newargs = args[1:]
+            newvec = vec + [(var, val)]
+            newkwargs = kwargs
+            newkwargs["vec"] = newvec
+            for i in scangrid(*newargs, **newkwargs):
+                yield i
+            # TODO: in Python3 this can replace the extra for-loop above
+            # yield from scan(*newargs, **newkwargs):
+    else:
+        yield vec
+
+
 def mk_minvals(anchors):
     from numpy import array
     a = array(anchors)
