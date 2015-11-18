5540e7ccdfb249eb6f78ffe85ab5e8fc546cedd2
Andy Buckley <andy@insectnation.org>
2015-11-18 23:41:13 +0000

    Add ROOT file reading ability.

--- pyext/professor2/dataio.py
+++ pyext/professor2/dataio.py
@@ -8,12 +8,53 @@ def read_histos(path):
     "Load histograms from file, into a dict of path -> yoda.Histo[DataBin]"
     histos = {}
     if path.endswith(".root"):
-        ## Try ROOT
+        ## Try to use ROOT
+        # TODO: Could just use YODA for everything, including ROOT reading?
         try:
             import ROOT
-            raise Exception("Can't yet read ROOT format files!")
-        except:
-            print "Can't yet read ROOT format files... TODO!"
+            ROOT.gROOT.SetBatch(True)
+        except ImportError:
+            print "PyROOT not available... skipping"
+            return histos
+
+        def _getallrootobjs(d, basepath="/"):
+            "Recurse through a ROOT file/dir and generate (path, obj) pairs"
+            for key in d.GetListOfKeys():
+                kname = key.GetName()
+                if key.IsFolder():
+                    # TODO: -> "yield from" in Py3
+                    for i in _getallrootobjs(d.Get(kname), basepath+kname+"/"):
+                        yield i
+                else:
+                    yield basepath+kname, d.Get(kname)
+        try:
+            f = ROOT.TFile(path)
+            for rname, robj in _getallrootobjs(f):
+                bins = []
+                if robj.InheritsFrom("TH1"):
+                    # TODO: allow 2D histos
+                    if robj.InheritsFrom("TH2"):
+                        continue
+                    for ib in xrange(robj.GetBins()):
+                        xmin = robj.GetXaxis().GetBinLowEdge(ib+1)
+                        xmax = robj.GetXaxis().GetBinUpEdge(ib+1)
+                        y = robj.GetBinContent(ib+1)
+                        ey = robj.GetBinError(ib+1)
+                        bins.append(DataBin(xmin, xmax, y, ey))
+                    histos[rname] = Histo(bins, rname)
+                elif robj.InheritsFrom("TGraph"):
+                    for ip in xrange(robj.GetN()):
+                        x, y = ROOT.Double(), ROOT.Double()
+                        robj.GetPoint(ip, x, y)
+                        xmin = x - robj.GetErrorXlow(ip)
+                        xmax = x + robj.GetErrorXhigh(ip)
+                        ey = (robj.GetErrorXlow(ip) + robj.GetErrorXhigh(ip)) / 2.0
+                        bins.append(DataBin(xmin, xmax, y, ey))
+                histos[rname] = Histo(bins, rname)
+            f.close()
+        except Exception, e:
+            print "Can't load histos from ROOT file '%s': %s" % (path, e)
+
     # TODO: can do this more generically with a try..except block, but requires that yoda.read throws a *Python* exception
     elif any(path.endswith(ext) for ext in [".yoda", ".aida", ".flat"]):
         ## Try YODA (handles .yoda, .aida, and .flat)
