e4c134760a265103c2a3929133e5e70742b2c181
Andy Buckley <andy@insectnation.org>
2017-02-21 15:55:20 +0000

    Add some weight path instructions to the prof2-tune help output

--- pyext/professor2/dataio.py
+++ pyext/professor2/dataio.py
@@ -139,7 +139,7 @@ def read_rundata(dirs, pfname="params.dat", verbosity=1): #, formats="yoda,root,
     numruns = len(dirs)
     for num, d in enumerate(sorted(dirs)):
         run = os.path.basename(d)
-        if verbosity >= 2 or (verbosity >= 1 and (num % 100 == 99)): # or num == numruns-1)):
+        if verbosity >= 2 or (verbosity >= 1 and (num % 100 == 99 or num == 0)):
             pct = 100*(num+1)/float(numruns)
             print "Reading run '%s' data: %d/%d = %2.0f%%" % (run, num+1, numruns, pct)
         files = glob.glob(os.path.join(d, "*"))
