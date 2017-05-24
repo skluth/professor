3d698e415e560478822332cfe601f44403921fb4
Andy Buckley <andy@insectnation.org>
2017-05-24 13:01:49 +0100

    Fix ipol-building progress report, by enabling float-default division in ipol.py

--- pyext/professor2/ipol.py
+++ pyext/professor2/ipol.py
@@ -1,5 +1,6 @@
 # -*- python -*-
 
+from __future__ import division
 from professor2.core import *
 from professor2.histos import *
 
@@ -93,6 +94,7 @@ def mk_ipolhisto(histos, runs, paramslist, order, errmode=None, errorder=None):
 
 
 def mkStandardIpols(HISTOS, HNAMES, RUNS, PARAMSLIST, CFG):
+    # TODO: factorise into per-bin and per-histo functions
 
     BNAMES = []
     for hn in HNAMES:
@@ -101,9 +103,9 @@ def mkStandardIpols(HISTOS, HNAMES, RUNS, PARAMSLIST, CFG):
         for n in xrange(nbins):
             BNAMES.append([hn, n])
 
-    NBINS=len(BNAMES)
+    NBINS = len(BNAMES)
 
-    MSGEVERY = int(NBINS/100.);
+    MSGEVERY = int(NBINS/100.) if NBINS > 100 else 1;
 
     import sys
     import professor2 as prof
@@ -113,7 +115,7 @@ def mkStandardIpols(HISTOS, HNAMES, RUNS, PARAMSLIST, CFG):
             if q.empty():
                 break
             temp = q.get()
-            hn=temp[0]
+            hn = temp[0]
             histos = HISTOS[hn]
             n = temp[1]
             xmax = histos.values()[0].bins[n].xmax
@@ -132,14 +134,13 @@ def mkStandardIpols(HISTOS, HNAMES, RUNS, PARAMSLIST, CFG):
                 rdict[(hn,n)] = s
                 del s
             del ib #< pro-actively clear up memory
-            counter.value+=1
-            if counter.value==MSGEVERY:
-                counter.value=0
+            counter.value +=1
+            if counter.value == MSGEVERY:
+                counter.value = 0
                 sys.stderr.write('\rProgress: {0:.1%}'.format(len(rdict.keys())/NBINS))
 
-
-
-    print "\n\nParametrising %i objects...\n"%len(BNAMES)
+    # TODO: Printing and multiprocessing should happen under script control
+    print "\n\nParametrising %i objects...\n" % len(BNAMES)
     import time, multiprocessing
     # time1 = time.time()
 
@@ -148,15 +149,13 @@ def mkStandardIpols(HISTOS, HNAMES, RUNS, PARAMSLIST, CFG):
     manager = Manager()
     tempDict = manager.dict()
 
-    # This for the status --- modululs is too expensive
-    ndone=Value('i', 0)
+    # This for the status --- modulus is too expensive
+    ndone = Value('i', 0)
 
     ## The job queue
     q = multiprocessing.Queue()
 
     map(lambda x:q.put(x), BNAMES)
-
-
 
     ## Fire away
     workers = [multiprocessing.Process(target=worker, args=(q, tempDict, ndone)) for i in xrange(CFG["MULTI"])]
